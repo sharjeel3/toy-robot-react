9d631748e3e98987d6957d0cfca93b91f8618472
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-01 00:48:41 +1100

    Fix app crash unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/*
+Workaround for jsdom issue 1695.
+Follow https://github.com/jsdom/jsdom/issues/1695
+ */
+window.HTMLElement.prototype.scrollIntoView = function() {};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
